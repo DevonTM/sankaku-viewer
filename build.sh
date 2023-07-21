0c1476212d2556fc65f7075d4be8f7d76eccec0a
Irfan Ammar Afif <ipangcheater@gmail.com>
2023-07-21 20:08:18 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-env GOAMD64=v3 go build -o sankaku.exe -v -trimpath -ldflags "-s -w" ./cmd/main.go
-env GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o sankaku -v -trimpath -ldflags "-s -w" ./cmd/main.go
+NAME=sankaku-viewer
+OS=$1
+ARCH=$2
+
+if [[ "$ARCH" == "386" ]]; then
+    DIR="$NAME-$OS-x86"
+else
+    DIR="$NAME-$OS-$ARCH"
+fi
+
+if [[ "$OS" == "windows" ]]; then
+    NAME="$NAME.exe"
+fi
+
+# Build the project
+echo "Building the project..."
+mkdir -p $DIR
+env GOOS=$OS GOARCH=$ARCH go build -v -o $DIR/$NAME -trimpath -ldflags "-s -w" cmd/sankaku/main.go
+tar cf $DIR.tar $DIR --format=posix --owner=0 --group=0
+gzip -9 $DIR.tar
+rm -rf $DIR
