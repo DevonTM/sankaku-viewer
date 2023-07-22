18a0d8ce29463627f0f61e3fc72562b7c7ff1294
Irfan Ammar Afif <ipangcheater@gmail.com>
2023-07-22 17:36:32 +0700

    fix the proxy to work with the fasthttp client

--- client.go
+++ client.go
@@ -1,10 +1,14 @@
 package sankaku
 
 import (
+	"encoding/base64"
 	"errors"
+	"net"
 	"net/url"
+	"strings"
 
 	"github.com/valyala/fasthttp"
+	"golang.org/x/net/proxy"
 )
 
 var (
@@ -21,8 +25,63 @@ func UseProxy(addr string) error {
 	if err != nil {
 		return err
 	}
-	if URL.Scheme != "http" && URL.Scheme != "https" && URL.Scheme != "socks5" {
-		return errors.New("only http, https and socks5 proxy are supported")
+
+	switch URL.Scheme {
+	case "http":
+		client.Dial = dialerHTTPProxy(URL)
+	case "socks5", "socks5h":
+		client.Dial = dialerSOCKS5Proxy(URL)
+	default:
+		return errors.New("only http and socks5 proxy are supported")
 	}
+
 	return nil
+}
+
+func dialerHTTPProxy(p *url.URL) fasthttp.DialFunc {
+	authHeader := "\r\n"
+	if p.User != nil {
+		auth := base64.StdEncoding.EncodeToString([]byte(p.User.String()))
+		authHeader = "Proxy-Authorization: Basic " + auth + "\r\n\r\n"
+	}
+	host := p.Host
+	return func(addr string) (net.Conn, error) {
+		conn, err := net.Dial("tcp", host)
+		if err != nil {
+			return nil, err
+		}
+		_, err = conn.Write([]byte("CONNECT " + addr + " HTTP/1.1\r\nHost: " + addr + "\r\n" + authHeader))
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
+		buf := make([]byte, 512)
+		n, err := conn.Read(buf)
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
+		if !strings.HasPrefix(string(buf[:n]), "HTTP/1.1 200 Connection established") {
+			conn.Close()
+			return nil, errors.New("proxy error: \n" + string(buf[:n]))
+		}
+		return conn, nil
+	}
+}
+
+func dialerSOCKS5Proxy(p *url.URL) fasthttp.DialFunc {
+	var auth *proxy.Auth
+	if p.User != nil {
+		auth = &proxy.Auth{User: p.User.Username()}
+		if pass, ok := p.User.Password(); ok {
+			auth.Password = pass
+		}
+	}
+	dialer, err := proxy.SOCKS5("tcp", p.Host, auth, proxy.Direct)
+	if err != nil {
+		return nil
+	}
+	return func(addr string) (net.Conn, error) {
+		return dialer.Dial("tcp", addr)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/valyala/fasthttp v1.48.0
+	golang.org/x/net v0.12.0
 )
 
 require (
